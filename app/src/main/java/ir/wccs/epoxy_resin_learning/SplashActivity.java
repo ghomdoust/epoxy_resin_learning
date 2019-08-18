56f33958cca15bae23921bc079156ff157b93486
محمود قوم دوست <47427371+ghomdoust@users.noreply.github.com>
2019-08-18 20:22:16 +0430

    Splash_Animation_Complete

--- app/src/main/java/ir/wccs/epoxy_resin_learning/SplashActivity.java
+++ app/src/main/java/ir/wccs/epoxy_resin_learning/SplashActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -25,6 +28,32 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         },10000);
+
+
+
+        ImageView img1 = (ImageView) findViewById(R.id.img1);
+        ImageView img2 = (ImageView) findViewById(R.id.img2);
+        ImageView img3 = (ImageView) findViewById(R.id.img3);
+        ImageView img4 = (ImageView) findViewById(R.id.img4);
+        ImageView img5 = (ImageView) findViewById(R.id.img5);
+
+        Animation animation1= AnimationUtils.loadAnimation(this,R.anim.move_down);
+        img1.setAnimation(animation1);
+
+        Animation animation2= AnimationUtils.loadAnimation(this,R.anim.move_up);
+        img2.setAnimation(animation2);
+
+        Animation animation3= AnimationUtils.loadAnimation(this,R.anim.move_left);
+        img3.setAnimation(animation3);
+
+        Animation animation4= AnimationUtils.loadAnimation(this,R.anim.move_right);
+        img4.setAnimation(animation4);
+
+        Animation animation5= AnimationUtils.loadAnimation(this,R.anim.move_right);
+        img5.setAnimation(animation5);
+
+
+
 
     }
 }

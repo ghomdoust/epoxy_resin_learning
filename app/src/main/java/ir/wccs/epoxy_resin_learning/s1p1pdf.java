e66af014bd3d5769b56bd7ceed37997cddfa62bc
محمود قوم دوست <47427371+ghomdoust@users.noreply.github.com>
2019-08-31 01:23:04 +0430

    LIST_LEVEL5_btnPDF_for_PDF

---
+++ app/src/main/java/ir/wccs/epoxy_resin_learning/s1p1pdf.java
@@ -0,0 +1,13 @@
+package ir.wccs.epoxy_resin_learning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class s1p1pdf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.s1p1pdf);
+    }
+}

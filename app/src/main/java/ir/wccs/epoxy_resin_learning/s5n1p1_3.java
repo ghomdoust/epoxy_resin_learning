e6574975e6da5170b0c90f814bf027f4443a852b
محمود قوم دوست <47427371+ghomdoust@users.noreply.github.com>
2019-09-15 02:11:24 +0330

    LIST_LEVEL21_Season5_Links_Complete

---
+++ app/src/main/java/ir/wccs/epoxy_resin_learning/s5n1p1_3.java
@@ -0,0 +1,158 @@
+package ir.wccs.epoxy_resin_learning;
+
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class s5n1p1_3 extends AppCompatActivity {
+
+
+
+
+    WebView mWebView;
+    ProgressBar progressBar;
+
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.s5n1p1_3);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        mWebView = (WebView) findViewById(R.id.mWebView);
+
+        progressBar.setVisibility(View.VISIBLE);
+        mWebView.setWebViewClient(new s5n1p1_3.Browser_home());
+        mWebView.setWebChromeClient(new s5n1p1_3.MyChrome());
+        WebSettings webSettings = mWebView.getSettings();
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDisplayZoomControls(false);
+
+        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebView.setScrollbarFadingEnabled(false);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAppCacheEnabled(true);
+        loadWebsite();
+
+
+
+    }
+
+    private void loadWebsite() {
+        ConnectivityManager cm = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            mWebView.loadUrl("https://hw18.cdn.asset.aparat.com/aparat-video/cae7e5da93c44f6d39469d478efa3e2511415049-480p__19942.mp4");
+        } else {
+            mWebView.setVisibility(View.GONE);
+        }
+
+        mWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+
+                DownloadManager.Request myRequest = new DownloadManager.Request(Uri.parse(url));
+                myRequest.allowScanningByMediaScanner();
+                myRequest.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                DownloadManager myManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                myManager.enqueue(myRequest);
+
+                Toast.makeText(s5n1p1_3.this,"Your file is downloading...", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+    }
+
+    class Browser_home extends WebViewClient {
+
+        Browser_home() {
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            setTitle(view.getTitle());
+            progressBar.setVisibility(View.GONE);
+            super.onPageFinished(view, url);
+
+        }
+    }
+
+    private class MyChrome extends WebChromeClient {
+
+        private View mCustomView;
+        private WebChromeClient.CustomViewCallback mCustomViewCallback;
+        protected FrameLayout mFullscreenContainer;
+        private int mOriginalOrientation;
+        private int mOriginalSystemUiVisibility;
+
+        MyChrome() {}
+
+        public Bitmap getDefaultVideoPoster()
+        {
+            if (mCustomView == null) {
+                return null;
+            }
+            return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
+        }
+
+        public void onHideCustomView()
+        {
+            ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
+            this.mCustomView = null;
+            getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+            setRequestedOrientation(this.mOriginalOrientation);
+            this.mCustomViewCallback.onCustomViewHidden();
+            this.mCustomViewCallback = null;
+        }
+
+        public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback)
+        {
+            if (this.mCustomView != null)
+            {
+                onHideCustomView();
+                return;
+            }
+            this.mCustomView = paramView;
+            this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+            this.mOriginalOrientation = getRequestedOrientation();
+            this.mCustomViewCallback = paramCustomViewCallback;
+            ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+            getWindow().getDecorView().setSystemUiVisibility(3846);
+        }
+    }
+
+}
--- app/src/main/java/ir/wccs/epoxy_resin_learning/season5.java
+++ app/src/main/java/ir/wccs/epoxy_resin_learning/season5.java
@@ -1,9 +1,14 @@
 package ir.wccs.epoxy_resin_learning;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class season5 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +16,59 @@ public class season5 extends AppCompatActivity {
         setContentView(R.layout.season5);
 
 
-}
+
+
+        final Button s1n1 = (Button) findViewById(R.id.btns1n1);
+        final Button s1n2 = (Button) findViewById(R.id.btns1n2);
+        final Button s1n3 = (Button) findViewById(R.id.btns1n3);
+        final Button s1n4 = (Button) findViewById(R.id.btns1n4);
+        final Button s1n5 = (Button) findViewById(R.id.btns1n5);
+
+
+
+
+        s1n1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season5.this,s5n1p1_1.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season5.this,s5n1p1_2.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season5.this,s5n1p1_3.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season5.this,s5n1p1_4.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season5.this,s1n1p1_5.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
 }

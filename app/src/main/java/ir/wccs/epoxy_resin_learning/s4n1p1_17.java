62568a63ab74bb6248bef7f5af78c9e983d3d446
محمود قوم دوست <47427371+ghomdoust@users.noreply.github.com>
2019-09-24 12:42:37 +0330

    LIST_LEVEL29_Season4_Second

---
+++ app/src/main/java/ir/wccs/epoxy_resin_learning/s4n1p1_17.java
@@ -0,0 +1,13 @@
+package ir.wccs.epoxy_resin_learning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class s4n1p1_17 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.s4n1p1_17);
+    }
+}
--- app/src/main/java/ir/wccs/epoxy_resin_learning/season4.java
+++ app/src/main/java/ir/wccs/epoxy_resin_learning/season4.java
@@ -1,14 +1,124 @@
 package ir.wccs.epoxy_resin_learning;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class season4 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.season4);
 
-}
+
+
+
+        final Button s1n1 = (Button) findViewById(R.id.btns1n1);
+        final Button s1n2 = (Button) findViewById(R.id.btns1n2);
+        final Button s1n3 = (Button) findViewById(R.id.btns1n3);
+        final Button s1n4 = (Button) findViewById(R.id.btns1n4);
+        final Button s1n5 = (Button) findViewById(R.id.btns1n5);
+        final Button s1n6 = (Button) findViewById(R.id.btns1n6);
+        final Button s1n7 = (Button) findViewById(R.id.btns1n7);
+        final Button s1n8 = (Button) findViewById(R.id.btns1n8);
+        final Button s1n9 = (Button) findViewById(R.id.btns1n9);
+        final Button s1n10 = (Button) findViewById(R.id.btns1n10);
+        final Button s1n11 = (Button) findViewById(R.id.btns1n11);
+        final Button s1n12 = (Button) findViewById(R.id.btns1n12);
+        final Button s1n13 = (Button) findViewById(R.id.btns1n13);
+        final Button s1n14 = (Button) findViewById(R.id.btns1n14);
+        final Button s1n15 = (Button) findViewById(R.id.btns1n15);
+        final Button s1n16 = (Button) findViewById(R.id.btns1n16);
+        final Button s1n17 = (Button) findViewById(R.id.btns1n17);
+        final Button s1n18 = (Button) findViewById(R.id.btns1n18);
+        final Button s1n19 = (Button) findViewById(R.id.btns1n19);
+        final Button s1n20 = (Button) findViewById(R.id.btns1n20);
+        final Button s1n21 = (Button) findViewById(R.id.btns1n21);
+        final Button s1n22 = (Button) findViewById(R.id.btns1n22);
+        final Button s1n23 = (Button) findViewById(R.id.btns1n23);
+        final Button s1n24 = (Button) findViewById(R.id.btns1n24);
+        final Button s1n25 = (Button) findViewById(R.id.btns1n25);
+        final Button s1n26 = (Button) findViewById(R.id.btns1n26);
+        final Button s1n27 = (Button) findViewById(R.id.btns1n27);
+        final Button s1n28 = (Button) findViewById(R.id.btns1n28);
+        final Button s1n29 = (Button) findViewById(R.id.btns1n29);
+        final Button s1n30 = (Button) findViewById(R.id.btns1n30);
+        final Button s1n31 = (Button) findViewById(R.id.btns1n31);
+        final Button s1n32 = (Button) findViewById(R.id.btns1n32);
+        final Button s1n33 = (Button) findViewById(R.id.btns1n33);
+
+
+
+
+        s1n1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season4.this,s3n1p1_1.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season4.this,s3n1p1_2.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season4.this,s3n1p1_3.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season4.this,s3n1p1_4.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season4.this,s3n1p1_5.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season4.this,s3n1p1_6.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season4.this,s3n1p1_7.class);
+                startActivity(intent);
+            }
+        });
+
+        s1n8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(season4.this,s3n1p1_8.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
 }

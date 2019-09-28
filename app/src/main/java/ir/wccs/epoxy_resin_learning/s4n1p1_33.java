120bd5e0eecb116036f454b55e074e753ad97d83
ghomdoust <ghomdoost@gmail.com>
2019-09-28 11:12:53 +0330

    Revert "LIST_LEVEL33_Season4_Sixth"
    This reverts commit 4b7e0ba965ba4ea0cba735d1b14649ec117ce56f.

--- app/src/main/java/ir/wccs/epoxy_resin_learning/s4n1p1_33.java
+++ app/src/main/java/ir/wccs/epoxy_resin_learning/s4n1p1_33.java
@@ -67,7 +67,7 @@ public class s4n1p1_33 extends AppCompatActivity {
         ConnectivityManager cm = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-            mWebView.loadUrl("https://hw19.cdn.asset.aparat.com/aparat-video/0c333746ec92e722ddc3568553dbe7c817129602-1080p__95772.mp4");
+            mWebView.loadUrl("https://hw17.cdn.asset.aparat.com/aparat-video/186de407b6503e79163bd12171a0e2c813104341-1080p__59476.mp4");
         } else {
             mWebView.setVisibility(View.GONE);
         }

0c83f002859e4ca4605081a9056507ed2531bca5
محمود قوم دوست <47427371+ghomdoust@users.noreply.github.com>
2020-09-03 14:55:07 +0430

    Logo_AboutUs_Complete

--- app/src/main/java/ir/wccs/epoxy_resin_learning/main_program.java
+++ app/src/main/java/ir/wccs/epoxy_resin_learning/main_program.java
@@ -1,62 +1,15 @@
 package ir.wccs.epoxy_resin_learning;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.github.barteksc.pdfviewer.PDFView;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class main_program extends AppCompatActivity {
-
-    PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sample_pdf_view);
+        setContentView(R.layout.main_program);
 
-        pdfView =(PDFView)findViewById(R.id.pdfView);
-
-        new RetrivePdfStream().execute("https://negargalleryart.ir/download/about_main_program.pdf");
-
-    }
-
-    class RetrivePdfStream extends AsyncTask <String,Void, InputStream>
-    {
-
-        @Override
-        protected InputStream doInBackground(String... strings) {
-            InputStream inputStream = null;
-            try{
-                URL url = new URL(strings[0]);
-                HttpURLConnection urlConnection=(HttpURLConnection)url.openConnection();
-                if(urlConnection.getResponseCode()==200)
-                {
-                    inputStream=new BufferedInputStream(urlConnection.getInputStream());
-
-                }
-            }
-
-            catch (IOException e)
-            {
-                return null;
-            }
-
-            return inputStream;
-
-        }
-
-        @Override
-        protected void onPostExecute(InputStream inputStream) {
-            pdfView.fromStream(inputStream).load();
-
-        }
     }
 }
